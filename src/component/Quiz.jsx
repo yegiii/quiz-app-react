4c563b2df12b6ffb866f7cad76752ee474032e8c
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-07-24 14:31:56 +0330

    Add logic when questions are end

--- src/component/Quiz.jsx
+++ src/component/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswer) => {
@@ -12,12 +15,24 @@ export default function Quiz() {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="completed quiz image" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuflledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuflledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuflledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
