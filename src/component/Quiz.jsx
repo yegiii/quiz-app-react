587210b36838585c122d5883ab5da601aeb8b8ff
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-07-24 12:57:29 +0330

    Show questions and answers in Quiz.jsx

--- src/component/Quiz.jsx
+++ src/component/Quiz.jsx
@@ -1,8 +1,31 @@
 import { useState } from "react";
 
+import QUESTIONS from "../questions";
 export default function Quiz() {
-  const [activeQuestion, setActiveQuestion] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
 
-  return <p>Currently active question</p>;
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
